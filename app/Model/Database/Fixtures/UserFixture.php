630d0f5c289916b6bdd256333ea2d483130fb2ca
Jakub Vitasek <me@jvitasek.cz>
2021-05-03 13:27:35 +0200

    Edited fixtures + added sample logo

--- app/Model/Database/Fixtures/UserFixture.php
+++ app/Model/Database/Fixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Database\Fixtures;
 
+use App\Model\Database\Entity\Account;
 use App\Model\Database\Entity\User;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -14,7 +15,11 @@ class UserFixture implements FixtureInterface, OrderedFixtureInterface {
 	 * @param ObjectManager $manager
 	 */
 	public function load(ObjectManager $manager): void {
+		/** @var Account $account */
+		$account = $manager->getRepository(Account::class)->find(1);
+
 		$user = new User();
+		$user->addAccount($account);
 		$user->setState(User::STATE_ACTIVATED);
 		$user->setEmail('admin@sample.com');
 		$user->setPassword('$2b$10$uu.7ssB2nxVUMKG7fZShQOtwe3N8/2I0E5niOL5OzXotpW7TPcjE2'); // the password is: secret
