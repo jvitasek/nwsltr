f7d08eeb9ad6631bcc26796474c4d8e44f597a71
Jakub Vitasek <me@jvitasek.cz>
2021-05-03 13:22:51 +0200

    Added fixtures

---
+++ app/Model/Database/Fixtures/AccountFixture.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace App\Model\Database\Fixtures;
+
+use App\Model\Database\Entity\Account;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AccountFixture implements FixtureInterface, OrderedFixtureInterface {
+
+	/**
+	 * Load data fixtures with the passed ObjectManager
+	 * @param ObjectManager $manager
+	 */
+	public function load(ObjectManager $manager): void {
+		$account = new Account();
+		$account->setTitle('Sample Account');
+		$account->setWebsiteUrl('https://google.com');
+		$account->setShowResubscribeButton(true);
+		$account->setShowUnsubscribeFeedbackForm(true);
+		$account->setEmailFrom('sample@sample.com');
+		$account->setEmailFromTitle('Sample NWSLTR Account');
+		$account->setEmailReplyTo('sample@sample.com');
+		$account->setPrimaryColorHex('E74C3C');
+		$account->setSecondaryColorHex('ECF0F1');
+		$account->setLogo('/uploads/logo/sample.png');
+		$account->setSmtpHost('smtp.mailtrap.io');
+		$account->setSmtpUsername('74993315e661a6');
+		$account->setSmtpPassword('6df992897adb59');
+		$account->setSmtpSecure('tls');
+		$account->setSmtpPort('2525');
+		$manager->persist($account);
+		$manager->flush();
+	}
+
+	/**
+	 * Get the order of this fixture
+	 */
+	public function getOrder(): int {
+		return 1;
+	}
+
+}
---
+++ app/Model/Database/Fixtures/UserFixture.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace App\Model\Database\Fixtures;
+
+use App\Model\Database\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture implements FixtureInterface, OrderedFixtureInterface {
+
+	/**
+	 * Load data fixtures with the passed ObjectManager
+	 * @param ObjectManager $manager
+	 */
+	public function load(ObjectManager $manager): void {
+		$user = new User();
+		$user->setState(User::STATE_ACTIVATED);
+		$user->setEmail('admin@sample.com');
+		$user->setPassword('$2b$10$uu.7ssB2nxVUMKG7fZShQOtwe3N8/2I0E5niOL5OzXotpW7TPcjE2'); // the password is: secret
+		$user->setRole(User::ROLE_ADMIN);
+		$user->setFirstName('Test');
+		$user->setLastName('Tester');
+		$manager->persist($user);
+		$manager->flush();
+	}
+
+	/**
+	 * Get the order of this fixture
+	 */
+	public function getOrder(): int {
+		return 2;
+	}
+
+}
