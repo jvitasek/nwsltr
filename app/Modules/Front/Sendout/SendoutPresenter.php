a3c6c327089dd485d56187cd0acc6e105ed1f874
Jakub Vitásek <me@jvitasek.cz>
2021-11-25 20:29:22 +0000

    Pagination in sendout stats

--- app/Modules/Front/Sendout/SendoutPresenter.php
+++ app/Modules/Front/Sendout/SendoutPresenter.php
@@ -2,15 +2,16 @@
 
 namespace App\Modules\Front\Sendout;
 
+use App\Model\Database\Entity\Queue;
 use App\Model\Database\Entity\Sendout;
 use App\Modules\Front\DatagridPresenter;
-use Nette\Application\Attributes\Persistent;
 use Nette\Application\BadRequestException;
 
 final class SendoutPresenter extends DatagridPresenter
 {
 
 	private array $sendouts;
+	private array $queueItems;
 	private Sendout $sendout;
 
 	public function actionList(): void
@@ -19,6 +20,12 @@ final class SendoutPresenter extends DatagridPresenter
 		$qb
 			->join('p.mailing', 'm')
 			->andWhere('m.account = :selectedAccount')->setParameter('selectedAccount', $this->account);
+
+		$paginator = $this['pagination']->getPaginator();
+		$paginator->itemsPerPage = self::DEFAULT_ITEMS_PER_PAGE;
+		$paginator->itemCount = count($qb->getQuery()->getResult());
+		$qb->setMaxResults($paginator->itemsPerPage)->setFirstResult($paginator->offset);
+
 		$this->sendouts = $qb->getQuery()->getResult();
 	}
 
@@ -34,6 +41,17 @@ final class SendoutPresenter extends DatagridPresenter
 
 		if ($sendout !== null) {
 			$this->sendout = $sendout;
+			$qb = $this->initializeQueryBuilder(Queue::class);
+			$qb = $qb
+				->andWhere('p.sendout = :sendout')
+				->setParameter('sendout', $sendout);
+
+			$paginator = $this['pagination']->getPaginator();
+			$paginator->itemsPerPage = self::DEFAULT_ITEMS_PER_PAGE;
+			$paginator->itemCount = count($qb->getQuery()->getResult());
+			$qb->setMaxResults($paginator->itemsPerPage)->setFirstResult($paginator->offset);
+
+			$this->queueItems = $qb->getQuery()->getResult();
 		} else {
 			throw new BadRequestException();
 		}
@@ -42,6 +60,7 @@ final class SendoutPresenter extends DatagridPresenter
 	public function renderStatistics(): void
 	{
 		$this->template->sendout = $this->sendout;
+		$this->template->queueItems = $this->queueItems;
 		$this->template->openedCount = $this->sendout->getCountByOpened(true);
 		$this->template->notOpenedCount = $this->sendout->getCountByOpened(false);
 	}
